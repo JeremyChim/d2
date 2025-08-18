fd6f0272423a1ee15ee06249d0bc82db59ced92c
JeremyChim <jer888chim@outlook.com>
2025-08-18 22:52:17 +0800

    修改物品脚本代码

--- script/items.py
+++ script/items.py
@@ -1,27 +1,27 @@
 def change_midas(path, save_path, xp, gold):
     with open(path, 'r', encoding='utf-8') as f:
-        ls = f.readlines()
+        data = f.readlines()
 
-    ls_new = ls
-    for i, line in enumerate(ls, 0):
+    data_new = data.copy()
+    change_index_and_line = {}
+    for i, line in enumerate(data):
         if '// Hand of Midas' in line:
             print(i + 1, line, end='')
-            break
-
-    for i2, line in enumerate(ls[i:], i):
-        if 'xp_multiplier' in line:
-            ls_new[i2] = ls_new[i2].replace('"2.1"', f'"{xp}"')
-            print(i2 + 1, ls_new[i2], end='')
-            break
-
-    for i3, line in enumerate(ls[i2:], i2):
-        if 'bonus_gold' in line:
-            ls_new[i3] = ls_new[i3].replace('"160"', f'"{gold}"')
-            print(i3 + 1, ls_new[i3], end='')
-            break
+            for i2, line2 in enumerate(data[i:], i):
+                if 'xp_multiplier' in line2:
+                    line2_new = line2.replace('2.1', f'{xp}')
+                    change_index_and_line[i2] = line2_new
+                    print(i2 + 1, line2_new, end='')
+                if 'bonus_gold' in line2:
+                    line2_new = line2.replace('160', f'{gold}')
+                    change_index_and_line[i2] = line2_new
+                    print(i2 + 1, line2_new, end='')
+                    break
+    for index, line in change_index_and_line.items():
+        data_new[index] = line
 
     with open(save_path, 'w', encoding='utf-8') as f:
-        f.writelines(ls_new)
+        f.writelines(data_new)
 
 
 if __name__ == '__main__':
